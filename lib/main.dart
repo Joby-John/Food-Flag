3a6816987c4509916088b9d4a27345ecbbcc39e2
Joby-John <joby432john@gmail.com>
2024-01-27 19:33:33 +0530

    added title, installed google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:FoodFlag/pages/map_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: MapPage(),
     );
   }
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
@@ -14,7 +14,9 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: AppBar( title: Text('Food Flag', style: GoogleFonts.libreBaskerville(
+          textStyle: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold))
+        ,),centerTitle: false,
         backgroundColor: const Color.fromARGB(200, 55, 149, 112),
         leading: IconButton(onPressed: (){}, icon: const Icon(Icons.menu_rounded, size: 37)),
         actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.search, size: 37,))],
