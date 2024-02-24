c59f4911c51536dc897c7e8e86e0471b25763e87
Joby-John <joby432john@gmail.com>
2024-02-24 19:02:14 +0530

    added upi scan and check for upi in db for existence

--- lib/pages/pay.dart
+++ lib/pages/pay.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-
 class PayNRaise extends StatefulWidget {
   const PayNRaise({super.key});
 
@@ -15,24 +14,27 @@ class _PayNRaiseState extends State<PayNRaise> {
   TextEditingController _amountController = TextEditingController();
   bool _isButtonEnabled = false;
   String? rawValue;
-  String upiID =  "";
+  String upiID = "";
   String name = "";
-  DocumentReference restUsersDocRef =
-  FirebaseFirestore.instance.collection('restaurants').doc('restaurantUsers');
+  DocumentReference restUsersDocRef = FirebaseFirestore.instance
+      .collection('restaurants')
+      .doc('restaurantUsers');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pay and Raise", style: GoogleFonts.marcellus(
-            textStyle: const TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.black54)),
+        title: Text(
+          "Pay and Raise",
+          style: GoogleFonts.marcellus(
+              textStyle: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black54)),
         ),
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 46, 204, 113),
-        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
@@ -41,35 +43,74 @@ class _PayNRaiseState extends State<PayNRaise> {
             children: [
               // QR Code Scanner Widget
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.4, // Fixed height for QR code scanner
+                height: MediaQuery.of(context).size.height *
+                    0.4, // Fixed height for QR code scanner
                 child: MobileScanner(
-                  controller: MobileScannerController(detectionSpeed: DetectionSpeed.noDuplicates),
-                    onDetect: (capture){
-                    final List<Barcode> barcodes = capture.barcodes;
-                    for(final barcode in barcodes)
-                      {
+                    controller: MobileScannerController(
+                        detectionSpeed: DetectionSpeed.noDuplicates),
+                    onDetect: (capture) async {
+                      final List<Barcode> barcodes = capture.barcodes;
+                      for (final barcode in barcodes) {
                         rawValue = barcode.rawValue;
                       }
+                      setState(() {
+                        upiID = extractUPIId(rawValue!);
+                        name = extractName(rawValue!);
+                      });
+
+                      // checking for restaurant existence in db
+                      DocumentSnapshot snapshot = await restUsersDocRef.get();
+                      Map<String, dynamic>? data =
+                          snapshot.data() as Map<String, dynamic>?;
+
+                      if (data != null && !data.containsKey(upiID)) {
+                        // UPI ID does not exist, show an alert dialog
                         setState(() {
-                      upiID = extractUPIId(rawValue!);
-                      name = extractName(rawValue!);
-                    });
+                          upiID = "";
+                          name = "";
+                        });
+
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('UPI ID not found'),
+                              content: Text(
+                                  'The UPI ID $upiID does not exist in the database.'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text('OK'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
                     }), // Implement QR code scanner here
               ),
-          
+
               // Non-editable text fields
-              SizedBox(height: 21,),
+              SizedBox(
+                height: 21,
+              ),
               Text(
                 "UPI ID: $upiID",
                 style: TextStyle(fontSize: 16),
               ),
-              SizedBox(height: 21,),
+              SizedBox(
+                height: 21,
+              ),
               Text(
                 "Name: $name",
                 style: TextStyle(fontSize: 16),
               ),
-              SizedBox(height: 21,),
-          
+              SizedBox(
+                height: 21,
+              ),
+
               // Editable text field for numbers
               TextFormField(
                 controller: _amountController,
@@ -77,24 +118,29 @@ class _PayNRaiseState extends State<PayNRaise> {
                 decoration: InputDecoration(
                   labelText: "Enter a number",
                   border: OutlineInputBorder(),
-                  errorText: _isButtonEnabled ? null : "Please enter the amount",
+                  errorText:
+                      _isButtonEnabled ? null : "Please enter the amount",
                 ),
                 onChanged: (value) {
                   setState(() {
                     // Enable or disable the button based on whether the text field is empty
-                    _isButtonEnabled = value.isNotEmpty;
+                    if(upiID!="") { // if the upi id is not found in db
+                      _isButtonEnabled = value.isNotEmpty;
+                    }
                   });
                 },
               ),
-          
-              SizedBox(height: 30,),
-          
+
+              SizedBox(
+                height: 30,
+              ),
+
               // Button
               ElevatedButton(
                 onPressed: _isButtonEnabled
                     ? () {
-                  // Implement button functionality here
-                }
+                        // Implement button functionality here
+                      }
                     : null,
                 child: Text("Submit"),
               ),
@@ -105,7 +151,6 @@ class _PayNRaiseState extends State<PayNRaise> {
     );
   }
 }
-
 
 String extractUPIId(String text) {
   RegExp regExp = RegExp(r'pa=([\w\.-]+@[\w\.-]+)');
