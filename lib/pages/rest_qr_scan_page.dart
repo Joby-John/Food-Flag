ebeaa8fb40d549e7cd666edbb7f714db47e509ff
Joby-John <joby432john@gmail.com>
2024-10-18 02:18:37 +0530

    solved the camera problem

--- lib/pages/rest_qr_scan_page.dart
+++ lib/pages/rest_qr_scan_page.dart
@@ -9,130 +9,159 @@ class QrScan extends StatefulWidget {
   State<QrScan> createState() => _QrScanState();
 }
 
-class _QrScanState extends State<QrScan> {
+class _QrScanState extends State<QrScan> with SingleTickerProviderStateMixin {
   final TextEditingController _amount = TextEditingController();
+  final TextEditingController _pin = TextEditingController();
+  late MobileScannerController _cameraController = MobileScannerController();
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+
+    // Listen to tab changes
+    _tabController.addListener(() {
+      setState(() {
+        if (_tabController.index == 0) {
+          _cameraController.start();  // Start scanner on first tab
+        } else if(_tabController.index == 1){
+          _cameraController.start();   // Start scanner on second tab
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Scan Qr"),
-          centerTitle: true,
-          bottom: TabBar(
-            tabs: [Tab(icon: Icon(Icons.flag_circle),text: 'Raise Flag',),
-                   Tab(icon: Icon(Icons.delete_forever), text: 'Delete Flag',)],
-          ),),
-        body: TabBarView(
-          children: [
-            SingleChildScrollView(
-              child: SafeArea(
-        child:Center(child: Column(
-            children: [
-              Scanner(),
-              Container(
-                padding: const EdgeInsets.all(12.0),
-                width: 210,
-
-                child: TextField(
-                  controller: _amount,
-
-                  decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.money, size: 33,),
-                      labelText: 'Enter Amount',
-                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25))),
-                )
-                ),
-              ),
-              Container(
-                width: 160,
-                child: FloatingActionButton(onPressed: (){}, child: Text("Create Flag"),),
-              )
-            ],
-            ),
-            ),
-    ),
-            ),
-
-            SingleChildScrollView(
-              child: SafeArea(
-                child:Center(child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Scan Qr"),
+        centerTitle: true,
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(icon: Icon(Icons.flag_circle), text: 'Raise Flag'),
+            Tab(icon: Icon(Icons.verified), text: 'Verify and Donate'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          SingleChildScrollView(
+            child: SafeArea(
+              child: Center(
+                child: Column(
                   children: [
-                    Scanner(),
+                    Scanner(controller: _cameraController),
                     Container(
                       padding: const EdgeInsets.all(12.0),
                       width: 210,
-
                       child: TextField(
-                          controller: _amount,
-                          obscureText: true,
-                          style: TextStyle(
-                            fontSize: 21,
-
+                        controller: _amount,
+                        decoration: InputDecoration(
+                          suffixIcon: Icon(Icons.money, size: 33),
+                          labelText: 'Enter Amount',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(25)),
                           ),
-                          textAlign: TextAlign.center,
-
-
-                          decoration: InputDecoration(
-                            suffixIcon: Icon(Icons.password, size: 25,),
-                            labelText: "Enter Pin",
-                            border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25))),
-                          )
+                        ),
                       ),
                     ),
                     Container(
                       width: 160,
-                      child: FloatingActionButton(onPressed: (){}, child: Text("Delete Flag"),),
+                      child: FloatingActionButton(
+                        onPressed: () {},
+                        child: Text("Create Flag"),
+                      ),
                     )
                   ],
                 ),
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: SafeArea(
+              child: Center(
+                child: Column(
+                  children: [
+                    Scanner(controller: _cameraController),
+                    Container( ///change this to that pop up thing and it should display amount of the flag.
+                      padding: const EdgeInsets.all(12.0),
+                      width: 210,
+                      child: TextField(
+                        controller: _pin,
+                        obscureText: true,
+                        style: TextStyle(
+                          fontSize: 21,
+                        ),
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          suffixIcon: Icon(Icons.password, size: 25),
+                          labelText: "Enter Pin",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(25)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 160,
+                      child: FloatingActionButton(
+                        onPressed: () {},
+                        child: Text("Verify"),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            )
-          ],
-        ),
-
+            ),
+          ),
+        ],
       ),
-
     );
   }
 }
 
-class Scanner extends StatefulWidget {
-  const Scanner({super.key});
+class Scanner extends StatelessWidget {
+  final MobileScannerController controller;
+  const Scanner({super.key, required this.controller});
 
-  @override
-  State<Scanner> createState() => _ScannerState();
-}
-
-class _ScannerState extends State<Scanner> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.all(12),
-        padding: EdgeInsets.all(12),
-        width: 300,
-        height: 300,
-
-        child: Stack(
-          children: [
-            MobileScanner(onDetect: (Capture) {
-
+      margin: EdgeInsets.all(12),
+      padding: EdgeInsets.all(12),
+      width: 300,
+      height: 300,
+      child: Stack(
+        children: [
+          MobileScanner(
+            controller: controller,
+            onDetect: (capture) {
+              // Handle QR code detection here
             },
-            ),
-
-            Container(
-              width: 300,
-              height: 300,
-              decoration: BoxDecoration(
-                border: Border.all(
-                    color: Colors.lightGreen,
-                    width: 4.0
-                ),
+          ),
+          Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.lightGreen,
+                width: 4.0,
               ),
-            )
-          ],
-        )
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
